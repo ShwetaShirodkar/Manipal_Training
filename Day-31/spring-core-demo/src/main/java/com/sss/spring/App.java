e4cf5e84632fcf9d3397a4de8fb2088633eca206
ShwetaShirodkar <testtest86@gmail.com>
2023-04-03 12:12:24 +0530

    Commit

--- Day-31/spring-core-demo/src/main/java/com/sss/spring/App.java
+++ Day-31/spring-core-demo/src/main/java/com/sss/spring/App.java
@@ -1,7 +1,5 @@
 package com.sss.spring;
 
-
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,20 +10,36 @@ import com.sss.spring.util.FileUtil;
 public final class App {
 
     public static void normal() {
+        System.out.println("🔥 Providing Dependencies Normally 🔥");
+
         FileUtil util = new FileUtil();
 
         FileChecker checker = new FileChecker(util);
         checker.checkFileValidity();
     }
 
-    public static void withSpring() {
+    public static void withSpringJavaConfig() {
+        System.out.println("🔥 Providing Dependencies With Java Code 🔥");
         ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        String[] names = ctx.getBeanDefinitionNames();
+        for(String nm : names) {
+            System.out.println(" 👉 "+nm);
+        }
+
+        FileChecker checker = ctx.getBean(FileChecker.class);
+        checker.checkFileValidity();
+    }
+
+    public static void withSpringXmlConfig() {
+        System.out.println("🔥 Providing Dependencies With Xml Config 🔥");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
 
         FileChecker checker = ctx.getBean(FileChecker.class);
         checker.checkFileValidity();
     }
 
     public static void main(String[] args) {
-        withSpring();
+        withSpringXmlConfig();
     }
 }
