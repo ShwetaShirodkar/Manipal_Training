fbab414b564136533ce1eb6d3676ed317c6ff49d
ShwetaShirodkar <testtest86@gmail.com>
2023-04-05 12:15:47 +0530

    understanding springboot

--- Day-33/SpringBoot/src/main/java/com/sss/springboot/SpringBoot/service/CarService.java
+++ Day-33/SpringBoot/src/main/java/com/sss/springboot/SpringBoot/service/CarService.java
@@ -1,6 +1,7 @@
 package com.sss.springboot.SpringBoot.service;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.sss.springboot.SpringBoot.entity.Car;
@@ -8,12 +9,16 @@ import com.sss.springboot.SpringBoot.entity.Car;
 @Service
 public class CarService {
     
+    @Value("${ws}")
+    private String website;
+
     public void createNewCar(Long id, String make, String model) {
 
         Car car = new Car();
         car.setId(id);
         car.setMake(make);
         car.setModel(model);
+        System.out.println("Car Company Website: " + website);
         System.out.println(car.toString());
     }
 }
