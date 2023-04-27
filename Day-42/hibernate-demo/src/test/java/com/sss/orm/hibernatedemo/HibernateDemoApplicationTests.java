c0a7fbb7ff91fa8c0805f022ed4615c5428593e3
ShwetaShirodkar <testtest86@gmail.com>
2023-04-27 11:05:05 +0530

    commit

---
+++ Day-42/hibernate-demo/src/test/java/com/sss/orm/hibernatedemo/HibernateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sss.orm.hibernatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
